82b9411c9cef694fb25378792b3e349d60817b8f
Soukina Nhaila <soukinanhaila@gmail.com>
2026-03-24 16:22:11 +0100

    feat: add saluer and calculer_imc functions

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 # TP GitHub DevOps
+=======
+<<<<<<< HEAD
+# tp-github-devops
+TP GitHub - Cours DevOps
+=======
+# TP GitHub DevOps
+>>>>>>> b51baf9 (init: initialisation du projet tp-github-devops)
+>>>>>>> 62a8d67 (feat: add saluer and calculer_imc functions)
--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+<<<<<<< HEAD
 print('Hello GitHub')
+=======
+def saluer(nom):
+    return f"Bonjour depuis GitHub, {nom} "
+
+def calculer_imc(poids, taille):
+    return round(poids / (taille ** 2), 2)
+
+if __name__ == "__main__":
+    print(saluer("DevOps"))
+    print("IMC:", calculer_imc(70, 1.75))
+>>>>>>> 62a8d67 (feat: add saluer and calculer_imc functions)
