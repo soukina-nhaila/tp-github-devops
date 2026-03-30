61312a823ca19b3ad23f4ae8d1d6d12bf8f753d6
Soukina Nhaila <soukinanhaila@gmail.com>
2026-03-30 11:03:13 +0100

    test

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-<<<<<<< 
-print('Hello GitHub')
-=======
+
 def saluer(nom):
     return f"Bonjour depuis GitHub, {nom} "
 
@@ -10,7 +8,6 @@ def calculer_imc(poids, taille):
 if __name__ == "__main__":
     print(saluer("DevOps"))
     print("IMC:", calculer_imc(70, 1.75))
->>>>>>> 62a8d67 (feat: add saluer and calculer_imc functions)
 
 def formater_message(msg, majuscules=False):
     return msg.upper() if majuscules else msg.lower()
